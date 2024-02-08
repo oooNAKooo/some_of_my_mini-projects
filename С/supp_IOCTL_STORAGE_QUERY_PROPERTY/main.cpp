775b4783858ddbf38ed1657a2077e44328887d1e
Samuel <121157661+oooNAKooo@users.noreply.github.com>
2024-02-08 11:42:59 +0300

    update_to_C

--- С/supp_IOCTL_STORAGE_QUERY_PROPERTY/main.cpp
+++ С/supp_IOCTL_STORAGE_QUERY_PROPERTY/main.cpp
@@ -1,12 +1,9 @@
-#include <Windows.h>
 #include <stdio.h>
-#include <iostream> 
+#include <windows.h>
 
 int main() {
-    setlocale(0, "");
-
     // Открываем дескриптор устройства (замените "C:" на нужное устройство)
-    HANDLE volumeHandle = CreateFile(L"\\\\.\\C:", 0, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+    HANDLE volumeHandle = CreateFileA("\\\\.\\C:", 0, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
 
     if (volumeHandle != INVALID_HANDLE_VALUE) {
         // Подготовка запроса
@@ -16,9 +13,10 @@ int main() {
 
         // Выделение буфера для получения информации
         STORAGE_DEVICE_DESCRIPTOR deviceDescriptor;
-        
+        DWORD bytesReturned;
+
         // Вызываем DeviceIoControl для проверки поддержки запроса
-        if (DeviceIoControl(volumeHandle, IOCTL_STORAGE_QUERY_PROPERTY, &query, sizeof(query), &deviceDescriptor, sizeof(deviceDescriptor), NULL, NULL)) {
+        if (DeviceIoControl(volumeHandle, IOCTL_STORAGE_QUERY_PROPERTY, &query, sizeof(query), &deviceDescriptor, sizeof(deviceDescriptor), &bytesReturned, NULL)) {
             // Устройство поддерживает IOCTL_STORAGE_QUERY_PROPERTY
             wprintf(L"Устройство поддерживает запрос IOCTL_STORAGE_QUERY_PROPERTY.\n");
         } else {
